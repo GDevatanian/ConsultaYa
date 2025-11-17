44802f5e8e54abd5ce7602b258a60907e8abdb73
TolozaLeo <leo_eltolo@hotmail.com>
2025-11-16 21:32:22 -0300

    Navegacion basica y pantalla de buscar médico.
    Se agregó una navegación básica para cambiar entre layouts dentro de Main Activity.
    Se creó el layout de buscar_medico.

--- app/src/main/java/com/ar/consultaya/MainActivity.kt
+++ app/src/main/java/com/ar/consultaya/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.ar.consultaya
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mostrarHome()
+    }
+
+    private fun mostrarHome() {
         setContentView(R.layout.activity_main)
 
         findViewById<android.view.View>(R.id.btnBuscarMedico).setOnClickListener {
-            Toast.makeText(this, "Buscar médico", Toast.LENGTH_SHORT).show()
+            mostrarBuscarMedico()
         }
 
         findViewById<android.view.View>(R.id.btnMisTurnos).setOnClickListener {
@@ -38,11 +43,43 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<android.view.View>(R.id.navHome).setOnClickListener {
-            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            mostrarHome()
         }
 
         findViewById<android.view.View>(R.id.navBuscar).setOnClickListener {
-            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show()
+            mostrarBuscarMedico()
+        }
+
+        findViewById<android.view.View>(R.id.navTurnos).setOnClickListener {
+            Toast.makeText(this, "Turnos", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<android.view.View>(R.id.navPerfil).setOnClickListener {
+            Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun mostrarBuscarMedico() {
+        setContentView(R.layout.activity_buscar_medico)
+
+        findViewById<Button>(R.id.btnAgendar1).setOnClickListener {
+            Toast.makeText(this, "Agendar con Dr. Pepe Pepito", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.btnAgendar2).setOnClickListener {
+            Toast.makeText(this, "Agendar con Dra. Pepa Pepita", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.btnAgendar3).setOnClickListener {
+            Toast.makeText(this, "Agendar con Dr. Juan Perez", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<android.view.View>(R.id.navHome).setOnClickListener {
+            mostrarHome()
+        }
+
+        findViewById<android.view.View>(R.id.navBuscar).setOnClickListener {
+            mostrarBuscarMedico()
         }
 
         findViewById<android.view.View>(R.id.navTurnos).setOnClickListener {
